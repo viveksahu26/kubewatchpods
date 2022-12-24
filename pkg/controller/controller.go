500fe0fcefa0e8ac075f9aeb598424a8f7103ee0
viveksahu26 <vivekkumarsahu650@gmail.com>
2022-12-25 00:05:05 +0530

    updated controller package
    Signed-off-by: viveksahu26 <vivekkumarsahu650@gmail.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
-	"github.com/viveksahu26/kubewatchpods/config"
+	"github.com/viveksahu26/kubewatchpods/pkg/config"
+	"github.com/viveksahu26/kubewatchpods/pkg/pkg/event"
 	"github.com/viveksahu26/kubewatchpods/pkg/pkg/handlers"
-	"github.com/viveksahu26/kubewatchpods/pkg/utils"
+	"github.com/viveksahu26/kubewatchpods/pkg/pkg/utils"
 	api_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -18,6 +21,10 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 )
+
+const maxRetries = 5
+
+var serverStartTime time.Time
 
 // Event indicate the informerEvent
 type Event struct {
@@ -60,10 +67,10 @@ func Start(conf *config.Config, eventHandler handlers.Handler) {
 		informer := cache.NewSharedIndexInformer(
 			&cache.ListWatch{
 				ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
-					return kubeClient.CoreV1().Pods(conf.Namespace).List(options)
+					return kubeClient.CoreV1().Pods(conf.Namespace).List(context.Background(), options)
 				},
 				WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, error) {
-					return kubeClient.CoreV1().Pods(conf.Namespace).Watch(options)
+					return kubeClient.CoreV1().Pods(conf.Namespace).Watch(context.Background(), options)
 				},
 			},
 			&api_v1.Pod{},
@@ -83,7 +90,7 @@ func Start(conf *config.Config, eventHandler handlers.Handler) {
 
 // create a instance of particulr resource controller: contains both informer and workqueue
 // Each resource must have their controller
-func newResourceController(client kubernetes.Interface, handlers handlers.Handler, informer cache.SharedIndexInformer, resourceType string) *Controller {
+func newResourceController(client kubernetes.Interface, eventHandler handlers.Handler, informer cache.SharedIndexInformer, resourceType string) *Controller {
 	// create a queue
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
@@ -140,10 +147,130 @@ func (c *Controller) Run(stopCh <-chan struct{}) {
 	go c.informer.Run(stopCh)
 
 	if !cache.WaitForCacheSync(stopCh, c.HasSynced) {
-		utilruntime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		utilruntime.HandleError(fmt.Errorf("timed out waiting for caches to sync"))
 		return
 	}
 	c.logger.Info("Kubewatchpod controller synced and ready")
 
 	wait.Until(c.runWorker, time.Second, stopCh)
+}
+
+// HasSynced is required for the cache.Controller interface.
+func (c *Controller) HasSynced() bool {
+	return c.informer.HasSynced()
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+		// continue looping
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	newEvent, quit := c.queue.Get()
+
+	if quit {
+		return false
+	}
+	defer c.queue.Done(newEvent)
+	err := c.processItem(newEvent.(Event))
+	if err == nil {
+		// No error, reset the ratelimit counters
+		c.queue.Forget(newEvent)
+	} else if c.queue.NumRequeues(newEvent) < maxRetries {
+		c.logger.Errorf("Error processing %s (will retry): %v", newEvent.(Event).key, err)
+		c.queue.AddRateLimited(newEvent)
+	} else {
+		// err != nil and too many retries
+		c.logger.Errorf("Error processing %s (giving up): %v", newEvent.(Event).key, err)
+		c.queue.Forget(newEvent)
+		utilruntime.HandleError(err)
+	}
+
+	return true
+}
+
+/* TODOs
+- Enhance event creation using client-side cacheing machanisms - pending
+- Enhance the processItem to classify events - done
+- Send alerts correspoding to events - done
+*/
+
+func (c *Controller) processItem(newEvent Event) error {
+	obj, _, err := c.informer.GetIndexer().GetByKey(newEvent.key)
+	if err != nil {
+		return fmt.Errorf("error fetching object with key %s from store: %v", newEvent.key, err)
+	}
+	// get object's metedata
+	objectMeta := utils.GetObjectMetaData(obj)
+
+	// hold status type for default critical alerts
+	var status string
+
+	// namespace retrived from event key incase namespace value is empty
+	if newEvent.namespace == "" && strings.Contains(newEvent.key, "/") {
+		substring := strings.Split(newEvent.key, "/")
+		newEvent.namespace = substring[0]
+		newEvent.key = substring[1]
+	}
+
+	// process events based on its type
+	switch newEvent.eventType {
+	case "create":
+		// compare CreationTimestamp and serverStartTime and alert only on latest events
+		// Could be Replaced by using Delta or DeltaFIFO
+		if objectMeta.CreationTimestamp.Sub(serverStartTime).Seconds() > 0 {
+			switch newEvent.resourceType {
+			case "NodeNotReady":
+				status = "Danger"
+			case "NodeReady":
+				status = "Normal"
+			case "NodeRebooted":
+				status = "Danger"
+			case "Backoff":
+				status = "Danger"
+			default:
+				status = "Normal"
+			}
+			kbEvent := event.Event{
+				Name:      objectMeta.Name,
+				Namespace: newEvent.namespace,
+				Kind:      newEvent.resourceType,
+				Status:    status,
+				Reason:    "Created",
+			}
+			c.eventHandler.Handle(kbEvent)
+			return nil
+		}
+	case "update":
+		/* TODOs
+		- enahace update event processing in such a way that, it send alerts about what got changed.
+		*/
+		switch newEvent.resourceType {
+		case "Backoff":
+			status = "Danger"
+		default:
+			status = "Warning"
+		}
+		kbEvent := event.Event{
+			Name:      newEvent.key,
+			Namespace: newEvent.namespace,
+			Kind:      newEvent.resourceType,
+			Status:    status,
+			Reason:    "Updated",
+		}
+		c.eventHandler.Handle(kbEvent)
+		return nil
+	case "delete":
+		kbEvent := event.Event{
+			Name:      newEvent.key,
+			Namespace: newEvent.namespace,
+			Kind:      newEvent.resourceType,
+			Status:    "Danger",
+			Reason:    "Deleted",
+		}
+		c.eventHandler.Handle(kbEvent)
+		return nil
+	}
+	return nil
 }
