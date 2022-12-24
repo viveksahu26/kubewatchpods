b19762250ae90894118340cf052d8a710af3b125
viveksahu26 <vivekkumarsahu650@gmail.com>
2022-12-24 14:22:06 +0530

    Added config struct
    Signed-off-by: viveksahu26 <vivekkumarsahu650@gmail.com>

--- config/config.go
+++ config/config.go
@@ -4,3 +4,18 @@ package config
 type Resource struct {
 	Pod bool `json:"po"`
 }
+
+// Config struct contains kubewatch configuration
+type Config struct {
+	// Handlers know how to send notifications to specific services.
+	// Handler Handler `json:"handler"`
+
+	// Reason   []string `json:"reason"`
+
+	// Resources to watch.
+	Resource Resource `json:"resource"`
+
+	// For watching specific namespace, leave it empty for watching all.
+	// this config is ignored when watching namespaces
+	Namespace string `json:"namespace,omitempty"`
+}
